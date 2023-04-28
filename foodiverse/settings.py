c6df759ddac3f21a04aa1031159fedfae5027e10
Bachir Ahmed Rachid <99692801+Bachir-Ahmed-Rachid@users.noreply.github.com>
2023-04-28 11:44:12 +0100

    PostgreSQL Config+.env
    PostgreSQL Database configuration.
    .env+env-sample creation for protecting sensitive data

--- foodiverse/settings.py
+++ foodiverse/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 
 from pathlib import Path
+from decouple import config
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -20,10 +21,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "django-insecure-)%u+pkg8((nk75bw^c8q&+d@cih@e7brw#n^of8q@*&$j+w0n0"
+SECRET_KEY = config('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = config('DEBUG', default=False, cast=bool)
 
 ALLOWED_HOSTS = []
 
@@ -74,9 +75,13 @@ WSGI_APPLICATION = "foodiverse.wsgi.application"
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
 DATABASES = {
-    "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
+   "default": {
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': config('NAME'),
+        'USER': config('USER'),
+        'PASSWORD':config('PASSWORD'),
+        'HOST':config('HOST', default='localhost'),
+        'PORT': config('PORT', default=25, cast=int),
     }
 }
 
